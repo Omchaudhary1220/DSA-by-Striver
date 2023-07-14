8924bf63c6a480eaca10ac1d5cfc691c7e9b38c7
om kumar chaudhary <117508079+Omchaudhary1220@users.noreply.github.com>
2023-07-14 18:23:18 +0530

    Update Recursive_bubblesort.cpp

--- Recursion/Recursive_bubblesort.cpp
+++ Recursion/Recursive_bubblesort.cpp
@@ -11,9 +11,26 @@ void bubblesort(int arr[],int s,int e){
     int l=s;
     while(l<e){
         if(arr[l]>arr[e]){
-            swap(&arr[l],&arr[e]);
+            swap(&arr[l],&arr[e]);   // we cannot optimise this code using flag because in this code we are just finding largest element and putting at last.
         }
         l++;
     }
     bubblesort(arr,s,e-1);
+}
+//optimising buuble sort by using a swapped flag variable.
+void bubblesort(int arr[],int n){
+   int swapped_flag=0;
+   if(n==1){
+       return;
+   }
+   for(int i=0;i<n-1;i++){
+       if(arr[i]>arr[i+1]){
+           swap(&arr[i],&arr[i+1]);
+           swapped_flag=1;
+       }
+   }
+   if(swapped_flag==0){
+       return;
+   }
+   bubblesort(arr,n-1);
 }
