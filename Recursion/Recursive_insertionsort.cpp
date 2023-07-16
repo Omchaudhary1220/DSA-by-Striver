171702f8634a87611b7d29690c564ae071ffc354
om kumar chaudhary <117508079+Omchaudhary1220@users.noreply.github.com>
2023-07-16 10:40:24 +0530

    Update Recursive_insertionsort.cpp

--- Recursion/Recursive_insertionsort.cpp
+++ Recursion/Recursive_insertionsort.cpp
@@ -6,16 +6,19 @@ void insertionsort(int arr[],int n,int i){
     }
     int temp=arr[i];
     int j=i-1;
+    int swapped=0;
     while(j>=0&&arr[j]>temp){
+        swapped=1;
         arr[j+1]=arr[j];
         j--;
     }
     arr[j+1]=temp;
+    if(swapped==1)
     insertionsort(arr,n,i+1);
     
 }
 int main(){
-    int arr[]={100,10,90,80,60,70,50,40,30,20,10};
+    int arr[]={50,40,30,10,20};
     int n=sizeof(arr)/sizeof(int);
     insertionsort(arr,n,1);
     for(auto i:arr){
