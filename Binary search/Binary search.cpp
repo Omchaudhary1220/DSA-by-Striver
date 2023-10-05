de1c16a8da7d5e58914dcbc494606a606ddc1820
om kumar chaudhary <117508079+Omchaudhary1220@users.noreply.github.com>
2023-10-05 10:54:25 +0530

    Create Binary search.cpp

---
+++ Binary search/Binary search.cpp
@@ -0,0 +1,18 @@
+int search(vector<int>& nums, int target) {
+    int s=0,e=nums.size()-1;
+    if(target<nums[0]||target>nums[e])return -1;
+    int mid=s+(e-s)/2;
+    while(s<=e){
+        if(nums[mid]==target){
+            return mid;
+        }
+        else if(nums[mid]<target){
+            s=mid+1;
+        }
+        else{
+            e=mid-1;
+        }
+      mid=s+(e-s)/2;
+    }
+    return -1;
+}
