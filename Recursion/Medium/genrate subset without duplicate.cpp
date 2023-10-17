90826b5fbb1f8a9b08fcf24946b8cd656510b29e
om kumar chaudhary <117508079+Omchaudhary1220@users.noreply.github.com>
2023-10-17 15:00:03 +0530

    Create genrate subset without duplicate.cpp

---
+++ Recursion/Medium/genrate subset without duplicate.cpp
@@ -0,0 +1,41 @@
+class Solution {
+private:
+    void unique_subset(vector<int>nums,vector<int>&temp,int ind,vector<vector<int>>&ans){
+        ans.push_back(temp);
+        for(int i=ind;i<nums.size();i++){
+            temp.push_back(nums[i]);
+            unique_subset(nums,temp,i+1,ans);
+            temp.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>ans;
+        vector<int>temp;
+        sort(nums.begin(),nums.end());
+        unique_subset(nums,temp,0,ans);
+        return ans;
+    }
+};
+
+// same apporch with different implemention
+class Solution {
+private:
+    void all_subset(vector<int>nums,vector<int>temp,int ind,vector<vector<int>>&ans){
+        if(ind>=nums.size()){
+            ans.push_back(temp);
+            return;
+        }
+        all_subset(nums,temp,ind+1,ans);
+        temp.push_back(nums[ind]);
+        all_subset(nums,temp,ind+1,ans);
+    }
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>ans;
+        vector<int>temp;
+        int ind=0;
+        all_subset(nums,temp,ind,ans);
+        return ans;
+    }
+};
