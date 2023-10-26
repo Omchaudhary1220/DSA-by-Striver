cb5778c4101fc07ae2f62ad1b15553574e01238c
om kumar chaudhary <117508079+Omchaudhary1220@users.noreply.github.com>
2023-10-26 19:17:39 +0530

    Create palindrome partitioning.cpp

---
+++ Recursion/Hard/palindrome partitioning.cpp
@@ -0,0 +1,32 @@
+class Solution {
+private:
+    bool ispal(string str,int s,int e){
+      while(s<=e){
+          if(str[s]!=str[e]) return 0;
+          s++;
+          e--;
+      }
+      return 1;
+    }
+    void getpalidrome(int ind,string s,vector<string>temp,vector<vector<string>>&ans){
+        if(ind==s.size()){
+            ans.push_back(temp);
+            return;
+        }
+        for(int i=ind;i<s.size();i++){
+            if(ispal(s,ind,i)){
+                temp.push_back(s.substr(ind,i-ind+1));
+                getpalidrome(i+1,s,temp,ans);
+                temp.pop_back();
+            }
+        }
+    }
+
+public:
+    vector<vector<string>> partition(string s) {
+       vector<vector<string>>ans;
+       vector<string>temp;
+       getpalidrome(0,s,temp,ans);
+       return ans; 
+    }
+};
