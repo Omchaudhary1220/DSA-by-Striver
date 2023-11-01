b99dc953f9c784c290befc715eeba223b8077066
om kumar chaudhary <117508079+Omchaudhary1220@users.noreply.github.com>
2023-11-01 19:53:43 +0530

    Update ArrayTo_list.cpp

--- Linked list/ArrayTo_list.cpp
+++ Linked list/ArrayTo_list.cpp
@@ -1,3 +1,4 @@
+Link:- https://www.codingninjas.com/studio/problems/introduction-to-linked-list_8144737
 Node* constructLL(vector<int>& arr) {
     Node* root=new Node(arr[0]);
     Node* temp=root;
