d38abd9d2052b497b972d6e5b638977f3d02c492
om kumar chaudhary <117508079+Omchaudhary1220@users.noreply.github.com>
2023-11-03 19:57:01 +0530

    Update N-queen.cpp

--- Recursion/Backtracking/N-queen.cpp
+++ Recursion/Backtracking/N-queen.cpp
@@ -1,4 +1,5 @@
 Link:- https://leetcode.com/problems/n-queens/
+   // solution
 class Solution {
 private:
    // function to check if it is not attacking 
@@ -45,6 +46,40 @@ public:
         vector<string>box(n,s);
         vector<vector<string>>ans;
         solve(0,n,box,ans);
+        return ans;
+    }
+};
+// solution with optimization
+class Solution {
+private:
+    void solve(int col,int n,vector<string>box,vector<int>rowdup,vector<int>upperdiagonal,vector<int>lowerdiagonal,vector<vector<string>>&ans){
+        if(col==n){
+            ans.push_back(box);
+            return;
+        }
+        for(int row=0;row<n;row++){
+            if(rowdup[row]==0&&lowerdiagonal[row+col]==0&&upperdiagonal[(n-1)+(row-col)]==0){
+                box[row][col]='Q';
+                rowdup[row]=1;
+                lowerdiagonal[row+col]=1;
+                upperdiagonal[(n-1)+(row-col)]=1;
+                solve(col+1,n,box,rowdup,upperdiagonal,lowerdiagonal,ans);
+                box[row][col]='.';
+                rowdup[row]=0;
+                lowerdiagonal[row+col]=0;
+                upperdiagonal[(n-1)+(row-col)]=0;
+            }
+        }
+    }
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        string s(n,'.');
+        vector<string>box(n,s);
+        vector<vector<string>>ans;
+        vector<int>rowdup(n,0);
+        vector<int>upperdiagonal(2*n-1,0);
+        vector<int>lowerdiagonal(2*n-1,0);;
+        solve(0,n,box,rowdup,upperdiagonal,lowerdiagonal,ans);
         return ans;
     }
 };
