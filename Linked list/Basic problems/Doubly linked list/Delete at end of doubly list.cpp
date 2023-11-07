3eba7e09fcad0cc18f10edea803688a18e514097
om kumar chaudhary <117508079+Omchaudhary1220@users.noreply.github.com>
2023-11-07 11:58:41 +0530

    Create Delete at end of doubly list.cpp

---
+++ Linked list/Basic problems/Doubly linked list/Delete at end of doubly list.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition of doubly linked list:
+ *
+ * struct Node {
+ *      int data;
+ *      Node *prev;
+ *      Node *next;
+ *      Node() : data(0), prev(nullptr), next(nullptr) {}
+ *      Node(int val) : data(val), prev(nullptr), next(nullptr) {}
+ *      Node(int val, Node *p, Node *n) : data(val), prev(p), next(n) {}
+ * };
+ *
+ *************************************************************************/
+
+Node * deleteLastNode(Node *head) {
+    if(head==NULL||head->next==NULL){
+        delete head;
+        head=NULL;
+        return head;
+    }
+    Node* temp=head;
+    while(temp->next->next!=NULL){
+        temp=temp->next;
+    }
+    delete temp->next;
+    temp->next=NULL;
+    return head;
+}
